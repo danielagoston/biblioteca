400f8ebad5f0d593b840bdff7542fb1a15d019a6
danielagoston <danielagostonschrotlin@gmail.com>
2024-02-13 15:49:15 -0300

    agregamos modelo Libro

--- apps/libros/models.py
+++ apps/libros/models.py
@@ -12,4 +12,42 @@ class Autor(models.Model):
     apellidos = models.CharField(max_length = 200, blank = False, null = False)
     nacionalidad = models.CharField(max_length = 100, blank = False, null = False)
     #textfield es un campo de texto extenso por eso no se aclara cuantos caracteres
-    descripcion = models.TextField(blank = False, null = False)  
+    descripcion = models.TextField(blank = False, null = False)
+    fecha_creacion = models.DateField(auto_now = True, auto_now_add = False)
+
+    class Meta:
+        #esto se pone para que en el admin aparezcan bien los nombres del modelo
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        #ordenar alfafebicamente
+        ordering = ['apellidos', 'nombre']
+
+    #con esta funcion hago que cada objeto creado se llame por su nombre en el admin
+    def __str__(self):
+        return self.apellidos + ', ' + self.nombre
+    
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key = True)
+    titulo = models.CharField(max_length = 255, blank = False, null = False)
+    fecha_publicacion = models.DateField(blank = False, null = False)
+    #auto_now indica la fecha que se modificó o crea automáticamente
+    fecha_creacion = models.DateField(auto_now = True, auto_now_add = False)
+    '''
+    la relación siempre tiene que llevar on_delete (excepto Clave foránea) 
+    para que una vez que se borre algú n dato también se borre hacia quién está hecha la relación
+     (y si se borra un autor se borra todo)
+    '''
+    #autor_id = models.OneToOneField(Autor, on_delete = models.CASCADE)
+    #autor_id = models.ForeignKey(Autor, on_delete = models.CASCADE)
+    autor_id = models.ManyToManyField(Autor)
+    
+    
+
+    class Meta:
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+        ordering = ['titulo']
+
+    def __str__(self):
+        return self.titulo
