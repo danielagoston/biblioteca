346c587a6686bbef18c80fb905368147f26eded5
danielagoston <danielagostonschrotlin@gmail.com>
2024-03-13 18:12:36 -0300

    redefinir funciones a clases en el CRUD, cambiar las respectivas URLS y agregar confirm delete y eliminar de manera lógica

--- apps/libro/forms.py
+++ apps/libro/forms.py
@@ -5,3 +5,39 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre','apellidos','nacionalidad','descripcion']
+        labels = {
+            'nombre':'Nombres del autor',
+            'apellidos':'Apellidos del autor',
+            'nacionalidad':'Nacionalidad del autor',
+            'descripcion':'Descripción del autor',
+        }
+        widgets = {
+            'nombre':forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder':'Ingrese los nombres del autor',
+                    'id':'nombre'
+                }
+            ),
+            'apellidos':forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder':'Ingrese los apellidos del autor',
+                    'id':'apellidos'
+                }
+            ),
+            'nacionalidad':forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder':'Ingrese la nacionalidad del autor',
+                    'id':'apellidos'
+                }
+            ),
+            'descripcion':forms.Textarea(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder':'Ingrese una breve descripción',
+                    'id':'apellidos'
+                }
+            ),
+        }
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -2,83 +2,37 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import AutorForm
 from .models import Autor
-from django.views.generic import View
+from django.views.generic import View, TemplateView, ListView, UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
 
-def home(request):
-    return render(request, 'index.html')
+class Inicio(TemplateView):
+    template_name = 'index.html'
 
-class Inicio(View):
-    pass
+class ListarAutor(ListView):
+    model = Autor
+    template_name = 'libro/listar_autor.html'
+    context_object_name = 'autores'
+    queryset = Autor.objects.filter(estado = True)
 
-def crearAutor(request):
-    # Comprueba si la solicitud es de tipo POST.
-    if request.method == 'POST':
-        # Si la solicitud es POST, crea un formulario de Autor con los datos proporcionados en la solicitud.
-        autor_form = AutorForm(request.POST)
-        # Comprueba si el formulario es válido.
-        if autor_form.is_valid():
-            # Si el formulario es válido, guarda los datos del nuevo autor en la base de datos.
-            autor_form.save()
-            # Redirige al usuario a la página de índice (o cualquier otra página que desees).
-            return redirect('libro:listar_autor')
-    else:
-        # Si la solicitud no es POST, crea un formulario vacío de Autor.
-        autor_form = AutorForm()
-    
-    # Renderiza la plantilla 'libros/crear_autor.html' y pasa el formulario de autor como contexto.
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form})
+class ActualizarAutor(UpdateView):
+    model = Autor
+    template_name = 'libro/crear_autor.html'
+    form_class = AutorForm
+    success_url = reverse_lazy('libro:listar_autor')
 
-def listarAutor(request):
-    # Recupera todos los autores cuyo estado sea verdadero (activo) desde la base de datos.
-    autores = Autor.objects.filter(estado=True)
-    
-    # Renderiza la plantilla 'listar_autor.html' y pasa la lista de autores recuperados como contexto.
-    return render(request, 'libro/listar_autor.html', {'autores': autores})
+class CrearAutor(CreateView):
+    model = Autor
+    form_class = AutorForm
+    template_name = 'libro/crear_autor.html'
+    success_url = reverse_lazy('libro:listar_autor')
 
+class EliminarAutor(DeleteView):
+    model = Autor
 
-def editarAutor(request, id):
-    # Inicialización de variables
-    autor_form = None
-    error = None
-    try:
-        # Intenta obtener el autor de la base de datos usando su ID
-        autor = Autor.objects.get(id=id)
-
-        # Si la solicitud es de tipo GET, muestra el formulario con los datos actuales del autor
-        if request.method == 'GET':
-            autor_form = AutorForm(instance=autor)
-        else:
-            # Si la solicitud es de tipo POST, procesa el formulario enviado por el usuario
-            autor_form = AutorForm(request.POST, instance=autor)
-            # Si el formulario es válido, guarda los cambios en la base de datos y redirige al usuario a la página de índice
-            if autor_form.is_valid():
-                autor_form.save()
-                return redirect('index')
-    # Maneja la excepción en caso de que el autor no exista
-    except ObjectDoesNotExist as e:
-        error = e
-     
-    # Renderiza el template 'libros/crear_autor.html' con el formulario del autor y el mensaje de error (si hay alguno)
-    return render(request, 'libro/crear_autor.html', {'autor_form': autor_form,'error':error})
-
-def eliminarAutor(request, id):
-    # Obtiene el autor correspondiente al ID proporcionado desde la base de datos.
-    autor = Autor.objects.get(id=id)
-    
-    # Verifica si la solicitud es de tipo POST (es decir, si se ha enviado el formulario de eliminación).
-    if request.method == 'POST':
-        # Elimina el autor de la base de datos.
-        #autor.delete()
-        
-        # Cambia el estado del autor a Falso.
-        autor.estado = False
-        # Guarda el cambio en la base de datos.
-        autor.save()
-        # Redirige al usuario a la vista listar_autor después de "eliminar" el autor.
-
-        # Redirige al usuario a la vista listar_autor después de eliminar el autor.
+    def post(self,request,pk,*args,**kwargs):
+        object = Autor.objects.get(id = pk)
+        object.estado = False
+        object.save()
         return redirect('libro:listar_autor')
-    
-    # Si la solicitud no es de tipo POST, renderiza la plantilla 'eliminar_autor.html' con los datos del autor.
-    return render(request,'libro/eliminar_autor.html',{'autor':autor})
