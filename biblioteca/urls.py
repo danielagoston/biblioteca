6764ec02e1d330d2056eb207ad4b2c26811975a3
danielagoston <danielagostonschrotlin@gmail.com>
2024-03-19 18:57:34 -0300

    login y logout, protección de urls

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.decorators import login_required
 from apps.libro.views import Inicio
-from django.contrib.auth import login,logout
-from django.contrib.auth.views import logout_then_login,LoginView
+from apps.usuario.views import Login, logoutUsuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('libro/',include(('apps.libro.urls','libro'))),
     path('',login_required(Inicio.as_view()), name = 'index'),
-    path('accounts/login/',LoginView.as_view(template_name = 'login.html'), name = 'login'),
-    path('logout/',logout_then_login, name ='logout'),
+    path('accounts/login/',Login.as_view(), name = 'login'),
+    path('logout/',login_required(logoutUsuario),name = 'logout'),
 ]
